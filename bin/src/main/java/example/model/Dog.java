501b4e354bba70ef7f17bed4e84cb16f829f262c
bearddan2000 <bearddan2000@yahoo.com>
2022-11-19 15:14:50 -0500

    initial commit

---
+++ bin/src/main/java/example/model/Dog.java
@@ -0,0 +1,22 @@
+package example.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String breed;
+    private String color;
+}
